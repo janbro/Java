98cad329817166c2a053c64ce2b398d33c931385
Max Miller <MaxMiller117@gmail.com>
2013-01-30 18:28:58 -0500

    More Stuff

--- BlackJackMethods.java
+++ BlackJackMethods.java
@@ -1,10 +1,14 @@
 package Java;
-
+import java.util.*;
 public class BlackJackMethods {
 	public static void main(String[] args){
-		boolean[] a=getPrimes(10);
-		for(boolean b:a)
-			System.out.println(b);
+//		boolean[] a=getPrimes(10);
+//		for(boolean b:a)
+//			System.out.println(b);
+//		for(String a:getCards())
+//			System.out.println(a);
+		factorInfo(24);
+		factorInfo(31);
 	}
 	public static boolean[] getPrimes(int n){
         boolean[] isPrime=new boolean[n+1];
@@ -15,5 +19,29 @@ public class BlackJackMethods {
                 for(int j=i;i*j<=n;j++)
                     isPrime[i*j]=false;
         return isPrime;
+	}
+	public static String[] getCards(){
+		String[] suit={"Clubs", "Diamonds", "Hearts", "Spades"};
+		String[] rank={"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
+		String[] cards=new String[suit.length*rank.length];
+		for(int i=0;i<suit.length;i++)
+			for(int j=0;j<rank.length;j++)
+				cards[i*rank.length+j]=rank[j]+" of "+suit[i];
+		return cards;
+	}
+	public static void factorInfo(int n){
+		ArrayList<Integer> factors=new ArrayList<Integer>();
+		factors.add(1);
+		int num=n;
+		for(int i=2;n>1;i++)
+			if(n%i==0){
+				factors.add(i);
+				n/=i;
+				i=2;
+			}
+		String not="";
+		if(factors.size()>2)
+			not="not ";
+		System.out.println(num+" has "+factors.size()+" factors "+num+" is "+not+"prime.");
 	}
 }
