4846eb7401705fd27b44e69328798fefcba2ce12
Max Miller <MaxMiller117@gmail.com>
2013-02-07 17:06:59 -0500

    'A' Turns on strobe lights.

--- LWJGLtesting.java
+++ LWJGLtesting.java
@@ -1,15 +1,17 @@
 package Java;
 
 import org.lwjgl.LWJGLException;
+import org.lwjgl.input.Keyboard;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
+import org.lwjgl.opengl.GL11;
 
-public class LWJGLtesting {
-	public static void main(String[] argv) {
+public class LWJGLtesting{
+	public static void main(String[] argv) throws InterruptedException{
 		LWJGLtesting displayExample = new LWJGLtesting();
 		displayExample.start();
 	}
-	public void start() {
+	public void start() throws InterruptedException{
 		try {
 			Display.setDisplayMode(new DisplayMode(800,600));
 			Display.create();
@@ -17,10 +19,27 @@ public class LWJGLtesting {
 			e.printStackTrace();
 			System.exit(0);
 		}
-		// init OpenGL here
-		while (!Display.isCloseRequested())
-			// render OpenGL here
+		GL11.glMatrixMode(GL11.GL_PROJECTION);
+		GL11.glLoadIdentity();
+		GL11.glOrtho(0, 800, 0, 600, 1, -1);
+		GL11.glMatrixMode(GL11.GL_MODELVIEW);
+		while (!Display.isCloseRequested()){
+			Keyboard.next();
+			boolean pressed=Keyboard.getEventKey()==Keyboard.KEY_A;
+			System.out.println(pressed);
+		    GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
+		    if(pressed)
+		    	GL11.glColor3f((float)Math.random(),(float)Math.random(),(float)Math.random());
+		    else
+		    	GL11.glColor3f(0.5f,0.5f,1.0f);
+		    GL11.glBegin(GL11.GL_QUADS);
+		    GL11.glVertex2f(100,100);
+			GL11.glVertex2f(100+200,100);
+			GL11.glVertex2f(100+200,100+200);
+			GL11.glVertex2f(100,100+200);
+		    GL11.glEnd();
 			Display.update();
+		}
 		Display.destroy();
 	}
 }
