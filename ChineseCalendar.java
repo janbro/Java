936181a81a58078c8f245100f4e72f3654680360
Max Miller <MaxMiller117@gmail.com>
2013-02-13 17:16:52 -0500

    Added ChineseCalandar.java and ChineseZodiac.java

---
+++ ChineseCalendar.java
@@ -0,0 +1,30 @@
+package Java;
+
+public class ChineseCalendar extends ChineseZodiac{
+	public String shortZodiac(int year){
+		String a=toZodiacString(year);
+		int comma=a.indexOf(",");
+		int paren=a.indexOf("(");
+		String animal=a.substring(0,comma);
+		String element=a.substring(paren+1,a.length()-1);
+		return animal+"/"+element;
+	}
+	public int getYear(int year, String animal, String element){
+		boolean P=false,Q=false;
+		for(String a:animals)
+			P=P||a.equals(animal);
+		for(String a:elements)
+			Q=Q||(a.indexOf(element)>-1);
+		if(!P||!Q)
+			throw new IllegalArgumentException();
+		int a=0,b=0;
+		while(!animals[a].equals(animal))
+			a++;
+		while(!(elements[b].indexOf(element)>-1))
+			b++;
+		int y=0;
+		for(int i=0;y<year;i++)
+			y=a+i*animals.length+b+i*elements.length;
+		return y;
+	}
+}
---
+++ ChineseZodiac.java
@@ -0,0 +1,30 @@
+package Java;
+
+public class ChineseZodiac {
+	private static final int startYear=1924;
+
+	protected static final String[] animals = {"rat","ox","tiger","rabbit",
+			"dragon","snake","horse","sheep","monkey","chicken","dog","pig"};
+
+	protected static final String[] elements ={"elder wood (fir)","younger wood (bamboo)",
+			"elder fire (burning wood)","younger fire (lamp flame)","elder earth (hill)",
+			"younger earth (plain)","elder metal (weapon)", "younger metal (kettle)",
+			"elder water (wave)", "younger water (brook)"};
+
+	public static String toZodiacString(int year){
+		year-=startYear;
+		String animal=animals[year%animals.length];
+		String element=elements[year%elements.length];
+		return animal+","+element;
+	}
+
+	//Yes, I included a main in this class. Notice, everything //in this class is static.
+
+	public static void main(String[] args){
+		System.out.println(toZodiacString(1925));
+		System.out.println(new ChineseCalendar().shortZodiac(1949));
+		System.out.println(toZodiacString(1936));
+		System.out.println(new ChineseCalendar().getYear(1924, "ox", "younger earth (plain)"));
+	}
+
+}
