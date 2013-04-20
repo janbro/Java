4bfe615604736d860ac333a4d56b9af59861b853
Max Miller <MaxMiller117@gmail.com>
2013-04-20 10:22:44 -0400

    Removed weird things in Player.java

--- Player.java
+++ Player.java
@@ -1,32 +1,17 @@
 package Java;
 
-import java.util.*;
-
 public class Player {
 	private Deck hand;
-	
 	public Player(){
 		hand = new Deck();
 		hand.clearDeck();
 	}
-	
 	public void addCard(Card a){
 		hand.addCard(a);
 	}
-	
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 0e7fa454d5c89459996fcf00318764e4dd7652c9
 	public void removeCard(Card a){
 		hand.takeCardOutOfDeck(a);
 	}
-	
-<<<<<<< HEAD
-=======
->>>>>>> 30bb20627c315b8353fc364794ea9b7b2f8623df
-=======
->>>>>>> 0e7fa454d5c89459996fcf00318764e4dd7652c9
 	public Deck getHand(){
 		return hand;
 	}
